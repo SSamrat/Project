be003906ae2f7c23afd0a1846cba3d3818630e17
Shamim <bsse0511@iit.du.ac.bd>
2014-04-19 11:32:14 +0600

    Initial commit minhaz

---
+++ src/run/MainApp.java
@@ -0,0 +1,13 @@
+package run;
+
+import playGame.ScoreFour;
+import player.ActivePlayer;
+
+public class MainApp {
+	public static void main(String[] args) {
+		ActivePlayer activePlayer1 = new ActivePlayer(1);
+		ActivePlayer activePlayer2 = new ActivePlayer(2);
+		
+		new ScoreFour(activePlayer1, activePlayer2);
+	}
+}
